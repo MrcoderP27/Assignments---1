d5d8f2d95c16d93d77bf200244d9f5e01f589751
Parth <parthpanjwani27@gamil.com>
2025-08-12 16:56:59 +0530

    task 1 completed

---
+++ Task_1.py
@@ -0,0 +1,19 @@
+num_1 = float(input("Enter first number: "))
+num_2 = float(input("Enter second number: "))
+
+#sum of two numbers
+sum = num_1 + num_2
+
+#diff of two numbers
+diff = num_1 - num_2
+
+#product of two numbers
+product = num_1 * num_2
+
+#division of two numbers
+division = num_1 / num_2
+
+print("Addition:" , sum)
+print("Subtraction:", diff)
+print("Multiplication:", product)
+print("Division:", division)
